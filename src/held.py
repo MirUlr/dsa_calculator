2fed1d187e5bfc98fae9d2c0db2e39819d5ca965
MirUlr <mirko.ulrich@tu-ilmenau.de>
2021-03-05 16:34:25 +0100

    Separate module for plot function and ordering imports.

--- src/held.py
+++ src/held.py
@@ -7,14 +7,12 @@ Created on Sat Jan 16 09:27:20 2021
 
 import json
 import multiprocessing
-import pandas as pd
 import pathlib
-from matplotlib import pyplot as plt
-
-from pylab import cm
 
 import numpy as np
+import pandas as pd
 
+import plottery
 
 class Held():
     """Baseclass for characters from the rpg `Das Shwarze Auge` (DSA).
@@ -401,8 +399,9 @@ class Held():
 
         # begin plotting process
         title='Verteilung der Qualitätsstufen von {}'.format(talent)
-        proc = multiprocessing.Process(target=self._plot_cube_of_success,
-                                       args=(table, title,))
+        proc = multiprocessing.Process(
+            target=plottery.plot_cube_of_success,
+            args=(table, title,))
         proc.start()
 
         # begin describing probabilities
@@ -1002,29 +1001,6 @@ class Held():
             if val in legal_response:
                 clean_read = True
         return val
-
-    @staticmethod
-    def _plot_cube_of_success(table, title):
-        header = list(table.columns)
-    
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        
-        x = table[header[0]]
-        y = table[header[1]]
-        z = table[header[2]]
-        q = table[header[3]]
-
-        colors = cm.Spectral(q / max(q))
-
-        ax.set_xlabel(header[0])
-        ax.set_ylabel(header[1])
-        ax.set_zlabel(header[2])
-        ax.set_title(title)
-
-        ax.scatter(x, y, z, c=colors, s=20, alpha=.5)
-        plt.show()
-
 
     @staticmethod
     def _tamper_designation(skill):
